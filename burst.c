e8b3600bc9a0b769d9cdfbb05ac9326ea8e9707c
cristinaemilia <budacristina9@gmail.com>
2017-05-08 21:48:40 -0400

    testing forking

--- burst.c
+++ burst.c
@@ -4,6 +4,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -13,55 +14,75 @@
 #define BLOCK 2048
 
 
+
+
 int main(int argc, char *argv[])
 {
-    int infd = STDIN_FILENO;
+    int readfd = STDIN_FILENO;
 
     // open a file
     if (argc > 1)
     {
-        infd = open(argv[1], O_RDONLY);
+        readfd = open(argv[1], O_RDONLY);
     }
 
-    int fcount = 0;
-    int linecount = 0;
-    char fname[512];
-    snprintf(fname, 512, "%s.%d", argv[1], ++fcount);
-    int writefd = open(fname, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    int fileCount = 0;
+    int lineCount = 0;
+    char fileName[512];
+   
     // read the opened file
-    char buf[BLOCK];
+    char readBuffer[BLOCK];
+    int lineBufferLen = BLOCK;
+    int lineBufferIndex = 0;
+    char* lineBuffer = (char*)malloc(BLOCK);
+   
 
-    int bytesread = 0;
-    while ((bytesread = read(infd, buf, BLOCK)) > 0)
+    int bytesRead = 0;
+    while ((bytesRead = read(readfd, readBuffer, BLOCK)) > 0)
     {
-        int newLineIndex = 0;
-		int i;
-        for (i = 0; i < bytesread; i++)
-        {
-            if (buf[i] == '\n')
-            {
-                if (++linecount < 500)
-                    continue;
-
-                if(fork() >= 0)
-                    continue;
-                    
-                linecount = 0;
-                write(writefd, buf + newLineIndex, i);
-                snprintf(fname, 512, "%s.%d", argv[1], ++fcount);
-                close(writefd);
-                writefd = open(fname, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-                newLineIndex = i;
-                continue;
-                
-            }
+        if(lineBufferIndex + bytesRead > lineBufferLen)
+        { 
+            lineBuffer = (char*) realloc(lineBuffer,lineBufferLen + BLOCK);
+            lineBufferLen = lineBufferLen + BLOCK; 
         }
 
-        write(writefd,buf+newLineIndex,bytesread - newLineIndex);
+        //copy the bytes read to the current readBufferfer
+        memcpy(lineBuffer+lineBufferIndex,readBuffer,bytesRead);
+        lineBufferIndex += bytesRead;
+
+        int i;
+        for (i = 0; i < bytesRead; i++)
+        {
+            if (readBuffer[i] == '\n')
+            {
+                int pid = fork();
+                if(pid < 0)
+                {
+                    fprintf(stderr, "ERROR: unable to fork\n");
+                    return 1;
+                }
+
+                snprintf(fileName, 512, "%s.%d", argv[1], ++fileCount);
+                int lineBufferOffset = (BLOCK - i);
+                if(pid > 0) {  
+                    //parent
+                   
+                    //set the line buffer index to 0, but keep using same line buffer
+                    memcpy(lineBuffer,lineBuffer + lineBufferIndex - lineBufferOffset, i);
+                    lineBufferIndex = i;
+                } else { 
+                    //child
+                    int writefd = open(fileName, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+                    write(writefd,lineBuffer, lineBufferIndex - lineBufferOffset);
+                    close(writefd);
+                }
+              
+            }
+        }
+        
     }
-    
-    close(writefd);
-    close(infd);
+
+    close(readfd);
 
     return 0;
 }
